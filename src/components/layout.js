de381e697ab130a05c3ac4b6963f3e26e88fbe68
jvirdy <jaskiranvirdy@hotmail.com>
2024-05-21 21:53:28 -0400

    Second commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-// Create page component
-// Functional Component
-const IndexPage = () => {
+const Layout = () => {
   return (
     <div>
       <title>Jaskiran Virdy</title>
       <h1>Hello meow meow!</h1>
       <p>How are you doing.........</p>
-      <Link to="https://www.linkedin.com/in/jaskiranvirdy/">LinkedIn</Link>
     </div>
   )
 }
 
-export default IndexPage
+export default Layout
